05f5dd0b8b909400a6afef4c3c4d0691dce68a91
l0509kb <l0509kb@gist.ac.kr>
2016-04-03 21:45:20 +0900

    shibal

---
+++ CoC/dataGen.cpp
@@ -0,0 +1,17 @@
+/* dataGen.cpp
+ *		Included headers: stdio, time
+ *		Descriptions:	generates data randomly with some given constraints
+ *						gives output in .txt file format to be used in other programs
+ */
+
+#include <stdio.h>		// NULL
+#include <time.h>		// time
+#include <stdlib.h>		// srand, rand
+#include <fstream>		// file output
+
+using namespace std;
+
+int main(void){
+	srand(time(NULL));
+	return 0;
+}
---
+++ CoC/hello.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello!!!" << endl;
+	cout << "Working?" << endl;
+	cout << "Yes!" << endl;
+	cout << "Hello Back!" << endl;
+	cout << "Smores!!" << endl;
+	cout << "Pull req." << endl;
+	cout << "changed" << endl;
+
+	cout << "kyubin test01" << endl;
+	return 0;
+}
